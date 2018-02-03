18aad8e0eb0d32a9bf24876ee50d7f9d6d1ec207
Alexander Bichuev <just.onmind@gmail.com>
2018-02-03 12:45:24 +0200

    Added keepAspect plugin

--- src/javascript/plugin-keep-aspect.js
+++ src/javascript/plugin-keep-aspect.js
@@ -1,18 +1,3 @@
-/*!
- * Onmind Plugins v0.0.1
- * Copyright 2018 Alexander Bichuev
- * Licensed under MIT
- * @author Alexander Bichuev
- * @version 0.0.1
- */
-
-;(function($){
-	$(function() {
-
-
-	});
-})(jQuery);
-
 		// plugin-keep-aspect.js
 		(function() {
 			'use strict';
