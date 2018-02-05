422cc4da823ee931b3236c2b3610da45d5252557
Alexander Bichuev <just.onmind@gmail.com>
2018-02-05 16:01:40 +0200

    Changed version number

--- build/js/onmind.plugins.js
+++ build/js/onmind.plugins.js
@@ -1,9 +1,9 @@
 /*!
- * Onmind Plugins v0.0.1
+ * Onmind Plugins v1.1.0
  * Copyright 2018 Alexander Bichuev
  * Licensed under MIT
  * @author Alexander Bichuev
- * @version 0.0.1
+ * @version 1.1.0
  */
 
 ;(function($){
